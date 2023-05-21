e9136db2386ac4588b572bab504afcc63c37e47f
user480 <SurajM9823@>
2023-05-21 10:49:54 +0530

    python basics

---
+++ main.py
@@ -0,0 +1,31 @@
+num1=10
+num2 =3
+# Arithmatic Operators
+print("num1 + num2 is",num1+num2)
+print("num1 - num2 is",num1-num2)
+print("num1 * num2 is",num1*num2)
+print("num1 / num2 is",num1/num2)
+print("num1 // num2 is",num1//num2)
+print("num1 ** num2 is",num1**num2)
+print("num1 % num2 is",num1%num2)
+
+# Assigment Operator
+a = 4
+b=6
+a +=2
+b -=2
+print(a)
+print(b)
+
+#Comparison Operators
+x=8
+y=3
+z=8
+print(x>3)
+print(y>3)
+print(x==z)
+
+#Logical operator
+
+print("Logical ex1",x==z and x<y)
+print("Logical ex2",x==z or x<y)
